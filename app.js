6eac3b3c83a4799248a6bd5325ef07cfe791c7d2
abdulmoizjawed <amoizj16@gmail.com>
2023-02-13 14:19:11 +0500

    get one tour added

--- app.js
+++ app.js
@@ -27,6 +27,17 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id/', (req, res) => {
+  const id = req.params.id * 1;
+
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   //   console.log(req.body);
 
